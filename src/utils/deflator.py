39cadae1d5ad3342aa4aa9cb8e1a71b83fa92d76
Keshav Krishnan <kkrishnan@parktudor.org>
2023-01-26 09:55:33 -0500

    test(deflator): cover deflator.py

--- src/utils/deflator.py
+++ src/utils/deflator.py
@@ -18,3 +18,13 @@ def fetch_cpi_series(fred_api_key: str, series_id: str = 'CPIAUCSL') -> pd.Serie
         series_id: FRED series identifier for CPI.
 
     Returns:
+        Monthly CPI-U index as pandas Series with datetime index.
+    """
+    if Fred is None:
+        raise ImportError("fredapi package required. Install via: pip install fredapi")
+    fred = Fred(api_key=fred_api_key)
+    cpi = fred.get_series(series_id)
+    logger.info(f"Fetched {len(cpi)} CPI observations from FRED ({series_id})")
+    return cpi
+
+
