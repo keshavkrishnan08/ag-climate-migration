d77caed4cdad746d3e067e1d16306b816e26d1ef
Keshav Krishnan <kkrishnan@parktudor.org>
2023-02-06 11:01:33 -0500

    test(geo): cover geo.py

--- src/utils/geo.py
+++ src/utils/geo.py
@@ -8,3 +8,13 @@ from loguru import logger
 EXCLUDED_STATE_FIPS = {'02', '15', '72'}
 
 # All 50 states + DC FIPS codes (CONUS = exclude AK, HI)
+CONUS_STATE_FIPS = {
+    f'{i:02d}' for i in range(1, 57)
+    if f'{i:02d}' not in EXCLUDED_STATE_FIPS and i not in (3, 7, 14, 43, 52)
+}
+
+
+def load_county_fips(path: str = None) -> pd.DataFrame:
+    """Load complete county FIPS table.
+
+    Args:
