b1a565aa42296178a1b44b28c36ab9980b12b296
Keshav Krishnan <kkrishnan@parktudor.org>
2023-02-09 01:36:50 -0500

    refactor(validation): clean up validation.py

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -20,3 +20,14 @@ def temporal_rolling_cv(
         Fold 1: train 1950-1985, val 1986-1990
         Fold 2: train 1950-1990, val 1991-1995
         Fold 3: train 1950-1995, val 1996-2000
+        Fold 4: train 1950-2000, val 2001-2005
+        Fold 5: train 1950-2005, val 2006-2010
+
+    Args:
+        years: Array of years for each observation.
+        n_folds: Number of CV folds.
+        train_start: First year of training data.
+        val_window: Number of years in each validation window.
+
+    Yields:
+        Tuples of (train_indices, val_indices) for each fold.
