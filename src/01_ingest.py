ec493925ef7439b3171ac7d8465f2908e8196be4
Keshav Krishnan <kkrishnan@parktudor.org>
2023-02-28 08:32:37 -0500

    test(01-ingest): cover 01_ingest.py

--- src/01_ingest.py
+++ src/01_ingest.py
@@ -8,3 +8,13 @@ Data sources per PRD Section 3.1:
     4. CMIP6 Climate Projections (ESGF Python API)
     5. USDA RMA Summary of Business (CSV)
     6. USDA Census of Agriculture (API + CSV)
+    7. USDA NASS Farmland Values (API)
+    8. Census ACS Rural Population (API)
+    9. BLS CPI Deflator (FRED API)
+    10. Rural Hospital Closures (CSV)
+    11. NCES School Enrollment (CSV)
+    12. USDA GIPSA Grain Elevators (CSV)
+"""
+
+import os
+import sys
