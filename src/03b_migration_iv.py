abd5be4f2c0b53f25c3c2c10feafbf33df8aabb5
Keshav Krishnan <kkrishnan@parktudor.org>
2023-05-17 22:05:49 -0500

    chore(03b-migration-iv): sync 03b_migration_iv.py

--- src/03b_migration_iv.py
+++ src/03b_migration_iv.py
@@ -8,3 +8,13 @@ Instrument: Weather-driven income shock. For each county-year, we compute:
     where yield_detrended = actual yield minus county-crop quadratic trend,
     acres_ic_bar = county-crop mean acres (fixed exposure),
     price_c = national commodity price.
+    This isolates the weather-driven component of farm revenue.
+
+Treatment: Farm income deviation from county baseline.
+    D_it = (income_it - income_i_bar) / income_i_bar
+    where income_it = sum_c [ yield_ict * acres_ict * price_c * deflator ]
+
+Outcome: Net outmigration rate from population change.
+    Y_it = -(pop_t - pop_{t-1}) / pop_{t-1}
+    Positive = population loss = net outmigration.
+
