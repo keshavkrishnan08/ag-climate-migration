eb80a2150e000fe22af2eb0337279a77ef98d07c
Keshav Krishnan <kkrishnan@parktudor.org>
2023-07-04 07:36:25 -0500

    chore(03b-migration-iv-v2): sync 03b_migration_iv_v2.py

--- src/03b_migration_iv_v2.py
+++ src/03b_migration_iv_v2.py
@@ -8,3 +8,13 @@ Approach 1: Net migration from population components
       - Remove counties with |pop_change| > 10% (boundary changes)
       - 3-year rolling population change to smooth noise
       - Weighted by baseline population (upweight large counties)
+
+Approach 2: Same-house inverse proxy
+    mobility_rate = 1 - (same_house / total_population)
+    = (mobility_total - same_house) / total_population
+    Interpretation: fraction of current residents who moved into the county
+    in the past year. Closely parallels Spec C/B but includes same-county movers.
+    Expected sign: positive (income -> more in-migration -> higher mobility rate).
+    Note: this is an IN-migration proxy, not pure out-migration, but captures
+    the same economic channel. Lower income -> fewer arrivals AND more departures.
+
