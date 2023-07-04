e00a5357069401f80d4af11926134d9619c6cf1a
Keshav Krishnan <kkrishnan@parktudor.org>
2023-07-04 16:56:33 -0500

    feat(03b-migration-iv-v2): extend 03b_migration_iv_v2.py

--- src/03b_migration_iv_v2.py
+++ src/03b_migration_iv_v2.py
@@ -18,3 +18,13 @@ Approach 2: Same-house inverse proxy
     Note: this is an IN-migration proxy, not pure out-migration, but captures
     the same economic channel. Lower income -> fewer arrivals AND more departures.
 
+Approach 3: Gross out-migration via population accounting
+    gross_out_rate = net_outmig_rate + in_migration_rate
+    = -(pop_t - pop_{t-1})/pop_{t-1} + true_diff_county_in_rate
+    Captures actual outflows regardless of inflow variation.
+
+Summary of all IV specifications:
+    Spec A  : net outmigration (raw pop change)              [original, p=0.49]
+    Spec A2 : net outmigration cleaned (excl boundary, 3yr)  [Approach 1]
+    Spec A3 : net outmigration pop-weighted                   [Approach 1 variant]
+    Spec B  : gross mobility rate (1 - same_house/pop)        [Approach 2]
