ef3ed4275611eb21a145950541cad5bc424475a0
Keshav Krishnan <kkrishnan@parktudor.org>
2023-07-11 16:59:07 -0500

    feat(03b-migration-iv-v2): extend 03b_migration_iv_v2.py

--- src/03b_migration_iv_v2.py
+++ src/03b_migration_iv_v2.py
@@ -318,3 +318,13 @@ def load_migration_outcomes():
     mig = pd.read_parquet(
         PROJECT_ROOT / "data/raw/census/acs_migration_data.parquet",
         columns=["fips", "year", "mobility_total", "moved_diff_county_same_state",
+                 "moved_diff_state", "moved_from_abroad"],
+    )
+    demo = pd.read_parquet(
+        PROJECT_ROOT / "data/raw/census/acs_county_demographics.parquet",
+        columns=["fips", "year", "total_population", "median_household_income"],
+    )
+
+    # Rename to true content (correct the mislabeling)
+    mig = mig.rename(columns={
+        "moved_diff_county_same_state": "same_house",           # B07001_002E
