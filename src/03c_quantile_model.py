11f94381cd3b178e3f2b09259e2258c598dd6827
Keshav Krishnan <kkrishnan@parktudor.org>
2023-08-01 10:27:12 -0500

    test(03c-quantile-model): cover 03c_quantile_model.py

--- src/03c_quantile_model.py
+++ src/03c_quantile_model.py
@@ -8,3 +8,13 @@ on correctly capturing downside scenarios.
 This script:
   1. Trains a LightGBM quantile model at alpha=0.10 (Q10) using same features
      and temporal split as the existing mean model.
+  2. Diagnoses the 2012 drought: Q10 prediction should be much more negative
+     than the mean model's -0.11.
+  3. Computes a tail risk premium per county (mean - Q10 gap) and adds it as
+     an additional stranded asset component.
+
+Temporal split (per CLAUDE.md):
+    train  : year <= 2009
+    val    : 2010-2016  (used as final training cutoff for test evaluation)
+    test   : 2017-2023
+
