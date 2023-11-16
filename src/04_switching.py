98d47f76002ee7af316585d1d0081a73bc45f603
Keshav Krishnan <kkrishnan@parktudor.org>
2023-11-16 13:21:17 -0500

    fix(04-switching): tighten 04_switching.py

--- src/04_switching.py
+++ src/04_switching.py
@@ -8,3 +8,13 @@ Architecture (PRD Section 5.2):
     - Outcome: P(switch from corn to sorghum in county c, year t)
     - Features: multi-year temperature trend, relative profitability,
       neighbor counties that already switched, farm debt
+    - Implementation: LightGBM classifier
+    - Calibration: Platt scaling for valid probabilities
+    - CRITICAL: switching probability must be monotone in temp trend
+      (hotter → more likely to switch away from heat-sensitive crops)
+
+Switching pairs (from config):
+    - [corn, soybeans]
+    - [corn, sorghum]
+    - [cotton, soybeans]
+    - [wheat_winter, wheat_spring]
