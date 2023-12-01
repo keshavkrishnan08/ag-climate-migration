177d1a105ad93891678ad65329a77125ba6b1ea4
Keshav Krishnan <kkrishnan@parktudor.org>
2023-11-30 22:37:43 -0500

    feat(05-project): add 05_project.py

---
+++ src/05_project.py
@@ -0,0 +1,10 @@
+"""Phase 4: Future projections — CMIP6 scenarios 2025-2050.
+
+Projects county-level crop yields using:
+1. Trained yield model (Phase 3A)
+2. Crop switching models (Phase 3B)
+3. CMIP6 climate projections downscaled to county level (pre-computed)
+
+Primary scenario: SSP2-4.5 (~RCP 4.5), +1.4-1.8°C by 2050.
+GCM ensemble: 5 CMIP6 models, median + 10-90th percentile uncertainty.
+"""
