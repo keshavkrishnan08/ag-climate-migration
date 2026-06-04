6318d54665672cd6cb26ebdde2ab5ad6cf3d2349
Keshav Krishnan <kkrishnan@parktudor.org>
2026-06-03 21:04:04 -0500

    fix(stranded-revision): tighten stranded_revision.py

--- src/revision/stranded_revision.py
+++ src/revision/stranded_revision.py
@@ -618,3 +618,13 @@ def build_state_table(county_df: pd.DataFrame) -> pd.DataFrame:
         "24": "Maryland", "25": "Massachusetts", "26": "Michigan",
         "27": "Minnesota", "28": "Mississippi", "29": "Missouri",
         "30": "Montana", "31": "Nebraska", "32": "Nevada",
+        "33": "New Hampshire", "34": "New Jersey", "35": "New Mexico",
+        "36": "New York", "37": "North Carolina", "38": "North Dakota",
+        "39": "Ohio", "40": "Oklahoma", "41": "Oregon", "42": "Pennsylvania",
+        "44": "Rhode Island", "45": "South Carolina", "46": "South Dakota",
+        "47": "Tennessee", "48": "Texas", "49": "Utah", "50": "Vermont",
+        "51": "Virginia", "53": "Washington", "54": "West Virginia",
+        "55": "Wisconsin", "56": "Wyoming",
+    }
+
+    df = county_df.copy()
